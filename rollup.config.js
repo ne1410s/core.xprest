2bc4bfdfc3c6e7921836651904e1504d9928eb4c
ne1410s <pgj646@gmail.com>
2020-06-06 23:31:27 +0100

    Deps Audit June '20

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,22 @@
-import rollup_bundler from './rollup.bundler.config';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
-export default [
-  rollup_bundler
-];
+// CommonJS and ES module builds
+export default {
+  input: 'src/index.ts',
+  external: [
+    'body-parser',
+    'cors',
+    'ejs',
+    'express',
+    'jws',
+    'path'
+  ],
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ],
+  plugins: [
+    typescript()
+  ]
+};
